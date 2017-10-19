2766df0e94f8f14e35457d85531f1111e42822ef
tony <anthony.kerz@gmail.com>
2017-10-19 10:14:12 -0400

    axios

--- test/ava/axios-test.js
+++ test/ava/axios-test.js
@@ -1,11 +1,11 @@
 import test from 'ava'
 import axios from 'axios'
-import {axios as axiosHelper} from '../../src'
+import {webHelpr} from '../../src'
 
 test('setToken', t => {
   const token = 's3cret'
-  axiosHelper.setToken(token)
+  webHelpr.setToken(token)
   t.is(`Bearer ${token}`, axios.defaults.headers.common.Authorization)
-  axiosHelper.unsetToken()
+  webHelpr.unsetToken()
   t.falsy(axios.defaults.headers.common.Authorization)
 })
