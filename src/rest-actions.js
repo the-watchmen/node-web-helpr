2de4f83b489fc84bd163e18fc5cd534f7e58322d
tony <anthony.kerz@gmail.com>
2019-06-08 23:42:19 -0400

    [babs] bump

--- src/rest-actions.js
+++ src/rest-actions.js
@@ -4,39 +4,39 @@ import debug from 'debug'
 const dbg = debug('lib:web-helpr:rest-actions')
 
 export default function({url, resource}) {
-  const _url = `${url}/${resource}`
-  const idUrl = id => `${_url}/${id}`
+	const _url = `${url}/${resource}`
+	const idUrl = id => `${_url}/${id}`
 
-  return {
-    index: async ({query}) => {
-      const result = await axios.get(_url, {params: query})
-      dbg('index: url=%o, query=%o, result=%o', _url, query, result)
-      return result
-    },
-    get: async ({id}) => {
-      const result = await axios.get(idUrl(id))
-      dbg('get: url=%o, id=%o, result=%o', _url, id, result)
-      return result.data
-    },
-    create: async ({data}) => {
-      const result = await axios.post(_url, data)
-      dbg('create: url=%o, data=%o, result=%o', _url, data, result)
-      return result
-    },
-    update: async ({id, data}) => {
-      const result = await axios.put(idUrl(id), data)
-      dbg('update: url=%o, id=%o, data=%o, result=%o', _url, id, data, result)
-      return result
-    },
-    patch: async ({id, data}) => {
-      const result = await axios.patch(idUrl(id), data)
-      dbg('patch: url=%o, id=%o, data=%o, result=%o', _url, id, data, result)
-      return result
-    },
-    delete: async ({id}) => {
-      const result = await axios.delete(idUrl(id))
-      dbg('delete: url=%o, id=%o, result=%o', _url, id, result)
-      return result
-    }
-  }
+	return {
+		index: async ({query}) => {
+			const result = await axios.get(_url, {params: query})
+			dbg('index: url=%o, query=%o, result=%o', _url, query, result)
+			return result
+		},
+		get: async ({id}) => {
+			const result = await axios.get(idUrl(id))
+			dbg('get: url=%o, id=%o, result=%o', _url, id, result)
+			return result.data
+		},
+		create: async ({data}) => {
+			const result = await axios.post(_url, data)
+			dbg('create: url=%o, data=%o, result=%o', _url, data, result)
+			return result
+		},
+		update: async ({id, data}) => {
+			const result = await axios.put(idUrl(id), data)
+			dbg('update: url=%o, id=%o, data=%o, result=%o', _url, id, data, result)
+			return result
+		},
+		patch: async ({id, data}) => {
+			const result = await axios.patch(idUrl(id), data)
+			dbg('patch: url=%o, id=%o, data=%o, result=%o', _url, id, data, result)
+			return result
+		},
+		delete: async ({id}) => {
+			const result = await axios.delete(idUrl(id))
+			dbg('delete: url=%o, id=%o, result=%o', _url, id, result)
+			return result
+		}
+	}
 }
--- src/standard-actions.js
+++ src/standard-actions.js
@@ -7,39 +7,39 @@ import getActions from './rest-actions'
 const dbg = debug('lib:web-helpr:standard-actions')
 
 export default function({url, resource}) {
-  const actions = getActions({url, resource})
+	const actions = getActions({url, resource})
 
-  return {
-    ...actions,
-    index: async ({query}) => {
-      const result = await actions.index({query: xformQuery({query})})
-      assert(result.data, `unable to extract data from result=${stringify(result)}`)
-      const {data} = result
+	return {
+		...actions,
+		index: async ({query}) => {
+			const result = await actions.index({query: xformQuery({query})})
+			assert(result.data, `unable to extract data from result=${stringify(result)}`)
+			const {data} = result
 
-      return {
-        data,
-        // x-total-count optional...
-        total: parseInt(result.headers['x-total-count']),
-        query
-      }
-    }
-  }
+			return {
+				data,
+				// X-total-count optional...
+				total: parseInt(result.headers['x-total-count']),
+				query
+			}
+		}
+	}
 }
 
 export function xformQuery({query}) {
-  dbg('xform-query: query=%o', query)
-  return _.transform(query, (result, val, key) => {
-    dbg('xform-query: result=%o, val=%o, key=%o', result, val, key)
-    if (key === 'sort' && val) {
-      val.field && (result.sort = val.isAscending ? val.field : `-${val.field}`)
-    } else if (_.isObject(val)) {
-      // https://stackoverflow.com/questions/47536618/is-there-any-way-to-have-redux-form-ignore-dots-in-a-field-name/47624233
-      const dotted = toDotNotation({target: {[key]: val}})
-      dbg('xform-query: dotted=%o', dotted)
-      const _key = _.keys(dotted)[0]
-      result[_key] = dotted[_key]
-    } else {
-      result[key] = val
-    }
-  })
+	dbg('xform-query: query=%o', query)
+	return _.transform(query, (result, val, key) => {
+		dbg('xform-query: result=%o, val=%o, key=%o', result, val, key)
+		if (key === 'sort' && val) {
+			val.field && (result.sort = val.isAscending ? val.field : `-${val.field}`)
+		} else if (_.isObject(val)) {
+			// https://stackoverflow.com/questions/47536618/is-there-any-way-to-have-redux-form-ignore-dots-in-a-field-name/47624233
+			const dotted = toDotNotation({target: {[key]: val}})
+			dbg('xform-query: dotted=%o', dotted)
+			const _key = _.keys(dotted)[0]
+			result[_key] = dotted[_key]
+		} else {
+			result[key] = val
+		}
+	})
 }
